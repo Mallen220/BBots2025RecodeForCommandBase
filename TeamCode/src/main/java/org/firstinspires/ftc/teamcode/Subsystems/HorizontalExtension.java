4884f0a131057aae13cbf525c7932e0ebff18c70
Mallen220 <allenmc220@gmail.com>
2025-08-07 16:49:41 +0200

    Refactor HorizontalExtension to use SolversLib CRServo instead of built in version
    Replaces usage of FTC SDK CRServo with seattlesolvers.solverslib.hardware.motors.CRServo. Updates initialization and method calls to match the new CRServo API, including setInverted and set methods. This improves compatibility with the custom hardware abstraction.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/HorizontalExtension.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/HorizontalExtension.java
@@ -1,8 +1,9 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
+import com.seattlesolvers.solverslib.hardware.motors.CRServo;
+
 import org.firstinspires.ftc.teamcode.Constants;
 
 public class HorizontalExtension extends SubsystemBase {
@@ -11,21 +12,20 @@ public class HorizontalExtension extends SubsystemBase {
 
   private final double MAX_POWER = 1;
   private final double MIN_POWER = -1;
-
   private final HardwareMap hwMap;
 
   public HorizontalExtension(HardwareMap hwMap) {
     this.hwMap = hwMap;
 
-    rightExtensionServo = hwMap.crservo.get(Constants.HorizontalConstants.RIGHT_EXTENSION_ID);
-    leftExtensionServo = hwMap.crservo.get(Constants.HorizontalConstants.LEFT_EXTENSION_ID);
-    rightExtensionServo.setDirection(CRServo.Direction.REVERSE);
+    rightExtensionServo = new CRServo(hwMap, Constants.HorizontalConstants.RIGHT_EXTENSION_ID);
+    leftExtensionServo = new CRServo(hwMap, Constants.HorizontalConstants.LEFT_EXTENSION_ID);
+    rightExtensionServo.setInverted(true);
   }
 
   public void setPower(double power) {
     if (power < MAX_POWER && power > MIN_POWER) {
-      leftExtensionServo.setPower(power);
-      rightExtensionServo.setPower(power);
+      leftExtensionServo.set(power);
+      rightExtensionServo.set(power);
     }
   }
 
